2657a31cb430960350053a689859979035bbb77c
minhquan1418 <lephamminhquan1418@gmail.com>
2018-04-05 09:25:33 +0700

    kich ban 06

---
+++ hello-jade/src/main/java/kichban06/FristAgent.java
@@ -0,0 +1,11 @@
+package kichban06;
+
+import jade.core.Agent;
+
+public class FristAgent extends Agent {
+	
+	@Override 
+	public void setup() {
+		System.out.println("Hello from first agent");
+	}
+}
---
+++ hello-jade/src/main/java/kichban06/SecondAgent.java
@@ -0,0 +1,11 @@
+package kichban06;
+
+import jade.core.Agent;
+
+public class SecondAgent extends Agent {
+	
+	@Override 
+	public void setup() {
+		System.out.println("Hello from second agent");
+	}
+}
